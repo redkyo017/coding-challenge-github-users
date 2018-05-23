8a8329ba65d1af4beb1d40e0a87c863e982ec99c
Duc Hung <hung.han@notabasement.com>
2018-05-23 10:11:22 +0700

    clean repo

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-
-router.get('/', require("./listUsers").default);
-router.post('/', require("./getGithubUsers").default);
-
-// module.exports = router;
-export default router
